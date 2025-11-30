9b01b2e6a5f8967beb558dd74bc2dfaade0b4632
stlwesh <stellawesh136@gmail.com>
2025-11-30 08:50:52 +0100

    nouveau back

---
+++ Mutuelle-Back/src/test/java/mutelleback/_5/Mutuelle/Back/MutuelleBackApplicationTests.java
@@ -0,0 +1,13 @@
+package mutelleback._5.Mutuelle.Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutuelleBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
