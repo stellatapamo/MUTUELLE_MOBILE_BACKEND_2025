ea68c364904fd8fae5f17f41986df2b1b3b32fc0
Franky Alongamo <alongamofranky@gmail.com>
2025-12-15 15:32:15 +0100

    update

--- src/main/java/com/mutuelle/mobille/controller/TransactionController.java
+++ src/main/java/com/mutuelle/mobille/controller/TransactionController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/emprunt-remboursement")
 @RequiredArgsConstructor
-@Tag(name = "mprunt-remboursement")
+@Tag(name = "emprunt-remboursement")
 public class TransactionController {
 
     private final TransactionService transactionService;
