db0e81a4d4ffef97e093da95290dcf27da4ceafd
Franky Alongamo <alongamofranky@gmail.com>
2026-01-22 22:07:31 +0100

    fixed

--- src/main/java/com/mutuelle/mobille/enums/TemplateMailsName.java
+++ src/main/java/com/mutuelle/mobille/enums/TemplateMailsName.java
@@ -1,21 +1,11 @@
 package com.mutuelle.mobille.enums;
 
 public enum TemplateMailsName {
-    WELCOME("welcome"),
-    PLAFOND_DEPASSE_ADMIN("alerte-plafond-depasse-admin"),
-    PLAFOND_DEPASSE_MEMBER("alerte-plafond-depasse-membre"),
-    SESSION_STARTED("session-start"),
-    SESSION_ENDED("alerte-end"),
-    SOLIDARITY_ADDED("solidarity-add"),
-    ASSISTANCE_GRANTED("alerte-plafond-depasse-admin");
-
-    private final String value;
-
-    TemplateMailsName(String value) {
-        this.value = value.toLowerCase();;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    WELCOME,
+    PLAFOND_DEPASSE_ADMIN,
+    PLAFOND_DEPASSE_MEMBER,
+    SESSION_STARTED,
+    SESSION_ENDED,
+    SOLIDARITY_ADDED,
+    ASSISTANCE_GRANTED
 }
